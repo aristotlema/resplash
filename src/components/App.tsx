80013c03866048238eeb054c413598cce1e6e4c0
aristotlema <aristotle.maule@hotmail.com>
2021-06-08 07:49:27 -0400

    setup react router basic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import NavBar from './NavBar';
+import HomePage from '../pages/HomePage';
 
 export default function App() {
-	return <div>App</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<NavBar />
+				<div>
+					<Route path="/" exact component={HomePage} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
